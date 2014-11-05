ffe7771e8c2d8b1bede71f8758cda08654c03612
witwolf <wo.bian.shi.mo0@gmail.com>
2014-11-05 11:44:40 +0800

    fix

--- CanJump.cpp
+++ CanJump.cpp
@@ -13,38 +13,22 @@ bool canJump(int A[],bool jumpFlag[],int curPos,int endPos);
 
 bool canJump(int A[], int n) {
     
-    bool *jumpFlag = new bool[n];
-    memset(jumpFlag, 0, n);
-    return canJump(A, jumpFlag,0,n-1);
+    
+    int coverPos = 0 ;
+    for(int i = 0 ; i <= coverPos && i < n ; i++ ){
+        if(coverPos < A[i] + i){
+            coverPos = A[i] + i;
+        }
+        
+        if(coverPos >= n-1){
+            return true;
+        }
+    }
+    
+    return false;
     
 }
 
-bool canJump(int A[] ,bool jumpFlag[],int curPos, int endPos){
-    if(curPos == endPos){
-        return true;
-    }
-    
-    // backward
-    int nextPos = curPos - A[curPos];
-    if(nextPos >= 0 && !jumpFlag[nextPos]){
-        jumpFlag[nextPos] = true;
-        if(canJump(A,jumpFlag,nextPos,endPos)){
-            return true;
-        }
-        jumpFlag[nextPos] = false;
-    }
-    
-    // forward
-    nextPos = curPos + A[curPos];
-    if(nextPos <= endPos && !jumpFlag[nextPos]){
-        jumpFlag[nextPos] = true;
-        if(canJump(A, jumpFlag,nextPos,endPos)){
-            return true;
-        }
-        jumpFlag[nextPos] = false;
-    }
-    return false;
-}
 
 int main(int argc,char **argv){
     int A1[] = {2,3,1,1,4};
